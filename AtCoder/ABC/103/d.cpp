1376c8bd007b4af5356563280e470b86cc8c843f
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2020-03-29 11:56:05 +0900

    abc103

---
+++ AtCoder/ABC/103/d.cpp
@@ -0,0 +1,114 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,s,n) for (int i = (int)s; i < (int)n; i++)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+#define lbidx(x, y) lower_bound(x.begin(), x.end(), y) - x.begin()
+#define ubidx(x, y) upper_bound(x.begin(), x.end(), y) - x.begin()
+#define llbidx(x, y, z) lower_bound(x.begin(), x.end(), z) - lower_bound(x.begin(), x.end(), y) // 二要素間の距離
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) printf("%.10f\n", x) // 小数点を10桁まで表示
+// debug用
+#define PrintVec(x) for (auto elementPrintVec: x) { cout << elementPrintVec << " "; } cout << endl;
+#define debug(x) cerr << #x << ": " << (x) << "\n";
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<int>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<vector<vector<PI>>> vvvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<vector<vector<PLL>>> vvvpl;
+
+int POWINT(int x, int n) {
+  int ret = 1;
+  rep(i, 0, n) ret *= x;
+  return ret;
+};
+
+ll POWLL(ll x, int n) {
+  ll ret = 1;
+  rep(i, 0, n) ret *= x;
+  return ret;
+};
+
+template<class T>
+inline bool chmax(T &a, T b) {
+  if(a < b) {
+    a = b;
+    return true;
+  }
+  return false;
+};
+
+template<class T>
+inline bool chmin(T &a, T b) {
+  if(a > b) {
+    a = b;
+    return true;
+  }
+  return false;
+};
+
+class ORD {
+  public:
+  int l, r;
+  ORD() {}
+  ORD(int l, int r): l(l), r(r) {}
+  bool operator < (const ORD &ord) const {
+    if (r == ord.r) {
+      return l < ord.l;
+    } else {
+      return r < ord.r;
+    }
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n, m;
+  cin >> n >> m;
+  vector<ORD> ords(m);
+  rep(i, 0, m) {
+    cin >> ords[i].l >> ords[i].r;
+  }
+
+  sort(All(ords));
+
+  int ans = 0;
+  set<int> cut;
+  rep(i, 0, m) {
+    auto ord = ords[i];
+    int r = ord.r;
+    int l = ord.l;
+    auto it_c = cut.upper_bound(l);
+    if (it_c == cut.end()) {
+      cut.insert(r);
+      ans++;
+    } else if (*it_c <= l) {
+      cut.insert(r);
+      ans++;
+    }
+  }
+
+  // cout << (int)cut.size() << endl;
+  cout << ans << endl;
+
+  return 0;
+};
