c3a376b0c106ab15b299c1bc46a342ecafadf8fc
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2020-06-01 20:57:21 +0900

    add ECR88

--- Codeforces/div2/ECR88/d.cpp
+++ Codeforces/div2/ECR88/d.cpp
@@ -113,16 +113,131 @@ void print(pair<T,U> &p){
   cout << p.first << " " << p.second << "\n";
 };
 
+// 0-indexed;
+template<class I, class BiOp>
+class SegTree {
+  int n;
+  std::vector<I> dat;
+  BiOp op;
+  I e;
+public:
+  SegTree(int n_, BiOp op, I e) : op(op), e(e) {
+    n = 1;
+    while (n < n_) { n *= 2; } // n is a power of 2
+    dat.resize(2 * n);
+    for (int i = 0; i < 2 * n - 1; i++) {
+      dat[i] = e;
+    }
+  }
+  /* ary[k] <- v */
+  void update(int k, I v) {
+    k += n - 1;
+    dat[k] = v;
+    while (k > 0) {
+      k = (k - 1) / 2;
+      dat[k] = op(dat[2 * k + 1], dat[2 * k + 2]);
+    }
+  }
+  void update_array(const vector<I> vals, int k=0) {
+    for (int i = 0; i < vals.size(); ++i) {
+      update(k + i, vals[i]);
+    }
+  }
+  /*
+    Updates all elements. O(n)
+   */
+  void update_all(const I *vals, int len) {
+    for (int k = 0; k < std::min(n, len); ++k) {
+      dat[k + n - 1] = vals[k];
+    }
+    for (int k = std::min(n, len); k < n; ++k) {
+      dat[k + n - 1] = e;
+    }
+    for (int b = n / 2; b >= 1; b /= 2) {
+      for (int k = 0; k < b; ++k) {
+	      dat[k + b - 1] = op(dat[k * 2 + b * 2 - 1], dat[k * 2 + b * 2]);
+      }
+    }
+  }
+  /* l,r are for simplicity */
+  I querySub(int a, int b, int k, int l, int r) const {
+    // [a,b) and  [l,r) intersects?
+    if (a >= b) return e;
+    if (r <= a || b <= l) return e;
+    if (a <= l && r <= b) return dat[k];
+    I vl = querySub(a, b, 2 * k + 1, l, (l + r) / 2);
+    I vr = querySub(a, b, 2 * k + 2, (l + r) / 2, r);
+    return op(vl, vr);
+  }
+  /* [a, b) */
+  I query(int a, int b) const {
+    return querySub(a, b, 0, 0, n);
+  }
+};
+
+struct MAX {
+  int operator() (int a, int b) const {
+    return max(a, b);
+  }
+};
+
+struct MIN {
+  int operator() (int a, int b) const {
+    return min(a, b);
+  }
+};
+
 const int MAX_N = 100005;
-vvi dp(MAX_N, vi(3));
+
+SegTree<int, MAX> rma(MAX_N, MAX(), -INF);
+SegTree<int, MIN> rmi(MAX_N, MIN(), INF);
 
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
 
   int n; cin >> n;
+  vi A(n);
+  vvi table(31);
+  rep(i, 0, n) {
+    cin >> A[i];
+    if (A[i] > 0) table[A[i]].pb(i);
+  }
 
+  vi sum(n);
+  sum[0] = A[0];
+  rep(i, 1, n) {
+    sum[i] += A[i] + sum[i-1];
+  }
 
+  rma.update_array(sum);
+  rmi.update_array(sum);
+
+  int ans = 0;
+
+  rep(i, 0, n) {
+    int a = A[i];
+    if (a <= 0) continue;
+    int r = n-1;
+    int l = -1;
+    rep(j, a+1, 31) {
+      int r_ = lbidx(table[j], i);
+      int l_ = r_-1;
+      if (r_ < table[j].size()) {
+        chmin(r, table[j][r_]-1);
+      }
+      if (l_ >= 0) {
+        chmax(l, table[j][l_]);
+      }
+    }
+    if (l == -1) {
+      chmax(ans, rma.query(i, r+1)-min(rmi.query(l, i),0)-a);
+    } else {
+      chmax(ans, rma.query(i, r+1)-rmi.query(l, i)-a);
+    }
+  }
+
+  cout << ans << endl;
 
   return 0;
 };
