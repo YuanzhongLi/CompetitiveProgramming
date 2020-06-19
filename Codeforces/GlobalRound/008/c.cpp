34c9a64e6d15a1f3ed57f7ae4a6f01b5ed3771e8
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2020-06-19 10:39:29 +0900

    add CGR 8

---
+++ Codeforces/GlobalRound/008/c.cpp
@@ -0,0 +1,179 @@
+#define LOCAL
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+#define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
+#define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+#define lbidx(x, y) lower_bound(x.begin(), x.end(), y) - x.begin()
+#define ubidx(x, y) upper_bound(x.begin(), x.end(), y) - x.begin()
+#define llbidx(x, y, z) lower_bound(x.begin(), x.end(), z) - lower_bound(x.begin(), x.end(), y) // dist between two elements
+// #define M_PI 3.14159265358979323846 // CF
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // 小数点を10桁まで表示
+#define endl "\n"
+#define Case(x) printf("Case #%d: ", x); // gcj
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<char> vch;
+typedef vector<vector<char>> vvch;
+
+constexpr ll LINF = 1001002003004005006ll;
+constexpr int INF = 1001001001;
+constexpr int n_max = 2e5+10;
+
+template<class T>
+inline bool chmax(T &a, T b) { if(a<b) { a=b; return true; } return false; };
+template<class T>
+inline bool chmin(T &a, T b) { if(a>b) { a=b; return true; } return false; };
+
+template<class T, class U>
+T POW(T x, U n) {T ret=1; while (n>0) {if (n&1) {ret*=x;} x*=x; n>>=1;} return ret;};
+
+// debug
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+string to_string(const string &s) {return '"' + s + '"';};
+string to_string(const char c) {return to_string((string) &c);};
+string to_string(bool b) {return (b ? "true" : "false");};
+template <size_t N>
+string to_string(bitset<N> v){
+  string res = "";
+  for(size_t i = 0; i < N; i++) res += static_cast<char>('0' + v[i]);
+  return res;
+};
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if(!first) res += ", ";
+    first = false; res += to_string(x);
+  }
+  res += "}";
+  return res;
+};
+template <typename A, typename B>
+string to_string(pair<A, B> p){return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
+
+void debug_out() {cerr << endl;};
+template<typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); };
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#else
+#define debug(...) 71
+#endif
+
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << " ";
+  print(forward<Tail>(tail)...);
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+};
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n; cin >> n;
+
+  vpi tmp;
+  rep(i, 0, (n+1)*2+1)  {
+    tmp.pb(make_pair(i, 4));
+  }
+
+  rep(i, 0, 5) {
+    tmp.pb(make_pair(0, i));
+  }
+
+  int row = 2;
+  rep(i, 0, n/2) {
+    rep(j, 2, 7) {
+      tmp.pb(make_pair(row, j));
+    }
+    tmp.pb(make_pair(row+1,2));
+    tmp.pb(make_pair(row+1,6));
+
+    rep(j, 2, 7) {
+      tmp.pb(make_pair(row+2, j));
+    }
+    row += 4;
+  }
+
+  rep(i, 0, row+1) {
+    tmp.pb(make_pair(i, 0));
+  }
+
+  if (n&1) {
+    rep(i, 0, 7) {
+      tmp.pb(make_pair(row, i));
+    }
+    rep(i, row, row+3) {
+      tmp.pb(make_pair(i, 6));
+    }
+    rep(i, 4, 7) {
+      tmp.pb(make_pair(row+2, i));
+    }
+  } else{
+    rep(i, 0, 5) {
+      tmp.pb(make_pair(row, i));
+    }
+  }
+
+  vvch grid((n+1)*2+1, vch(7, '.'));
+  for (auto pi: tmp) {
+    grid[pi.first][pi.second] = '#';
+  }
+  vpi ans;
+  rep(i, 0, (n+1)*2+1) {
+    rep(j, 0, 7) {
+      if (grid[i][j] == '#') ans.pb(make_pair(i, j));
+    }
+  }
+
+  cout << ans.size() << endl;
+  for (auto pi: ans) {
+    cout << pi.first << " " << pi.second << endl;
+  }
+  return 0;
+};
