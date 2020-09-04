d1fcb40441ae6a82992cfd5fb3d307c535226c58
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2020-09-04 13:26:23 +0900

    update abc127

--- AtCoder/ABC/127/f.cpp
+++ AtCoder/ABC/127/f.cpp
@@ -117,9 +117,99 @@ void print(vector<vector<T>> &df) {
   }
 };
 
+template<typename T>
+class BIT {
+public:
+  int n;
+  vector<T> dat;
+
+  BIT(int nin=0) {
+    n = nin;
+    dat.resize(n);
+    for (int i = 0; i < n; i++) dat[i] = 0;
+  }
+
+  // 0~iまでの和を求める
+  T sum(int i) {
+    T s = 0;
+    while (i >= 0) {
+      s += dat[i];
+      i = (i & (i+1)) - 1;
+    }
+    return s;
+  }
+
+  // [i, j]の和を求める
+  T sum_between(int i, int j){
+    if(i > j) return 0;
+    return sum(j) - sum(i-1);
+  }
+
+  // 一点更新
+  void add(int i, T x) {
+    while(i < n) {
+      dat[i] += x;
+      i |= i+1;
+    }
+  }
+
+  // a[0]+...+a[ret] >= x
+  int lower_bound(T x){
+    int ret = -1;
+    int k = 1;
+    while (2*k <= n) k <<= 1;
+    for( ;k>0; k>>=1){
+      if(ret+k < n && dat[ret+k] < x){
+        x -= dat[ret+k];
+        ret += k;
+      }
+    }
+    return ret + 1;
+  }
+};
+
 int main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
+  int Q; cin >> Q;
+  vi T(Q), A(Q), B(Q);
+  vi as;
+  rep(i, 0, Q) {
+    cin >> T[i];
+    if (T[i] == 1) {
+      cin >> A[i] >> B[i];
+      as.pb(A[i]);
+    }
+  }
+
+  // 圧縮
+  sort(All(as));
+  as.erase(unique(All(as)), as.end());
+  map<int, int> mp;
+  int sz = as.size();
+  rep(i, 0, sz) mp[as[i]] = i; // 値に対応するindex(小さい方から何番目か)
+
+  BIT<ll> bitnum(sz); // 何個かを管理するBIT
+  BIT<ll> bitsum(sz); // 和を管理するBIT
+  ll fix = 0; // 不変の部分
+  int all = 0; // 何個追加されたか(つまりq = 1が何回か)
+  rep(i, 0, Q) {
+    if (T[i] == 1) {
+      int ai = mp[A[i]];
+      bitsum.add(ai, A[i]);
+      bitnum.add(ai, 1);
+      fix += B[i];
+      all++;
+    } else {
+      ll ans = fix;
+      int idx = bitnum.lower_bound((all+1)/2);
+      ans += bitsum.sum_between(idx+1, sz-1);
+      ans -= as[idx] * bitnum.sum_between(idx+1, sz-1);
+      ans -= bitsum.sum_between(0, idx-1);
+      ans += as[idx] * bitnum.sum_between(0, idx-1);
+      cout << as[idx] << " " << ans << endl;
+    }
+  }
 
   return 0;
 };
