d6f6529d743bd17c6447146433540e81f6447e58
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2021-01-31 23:09:19 +0900

    update default

--- default/c.cpp
+++ default/c.cpp
@@ -5,6 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long
 #define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
 #define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
 #define ll long long
