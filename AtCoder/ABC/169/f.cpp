044bea9e8f75719d9444d0356bc46f993a73d2bb
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2021-02-23 13:18:30 +0900

    udpate abc 169 f

--- AtCoder/ABC/169/f.cpp
+++ AtCoder/ABC/169/f.cpp
@@ -1,10 +1,11 @@
-#define LOCAL
+// #define LOCAL
 #ifdef LOCAL
 #define _GLIBCXX_DEBUG
 #endif
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long
 #define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
 #define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
 #define ll long long
@@ -13,14 +14,11 @@ using namespace std;
 #define eb emplace_back
 #define All(x) x.begin(), x.end()
 #define Range(x, i, j) x.begin() + i, x.begin() + j
-#define lbidx(x, y) lower_bound(x.begin(), x.end(), y) - x.begin()
-#define ubidx(x, y) upper_bound(x.begin(), x.end(), y) - x.begin()
-#define llbidx(x, y, z) lower_bound(x.begin(), x.end(), z) - lower_bound(x.begin(), x.end(), y) // dist between two elements
 // #define M_PI 3.14159265358979323846 // CF
 #define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
 #define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
 #define Find(set, element) set.find(element) != set.end()
-#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // 小数点を10桁まで表示
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // print Decimal number 10 Rank
 #define endl "\n"
 #define Case(x) printf("Case #%d: ", x); // gcj
 
@@ -31,22 +29,24 @@ typedef vector<vector<int>> vvi;
 typedef vector<vector<vector<int>>> vvvi;
 typedef vector<ll> vl;
 typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
 typedef vector<PI> vpi;
 typedef vector<vector<PI>> vvpi;
 typedef vector<PLL> vpl;
 typedef vector<vector<PLL>> vvpl;
+typedef vector<char> vch;
+typedef vector<vector<char>> vvch;
 
-constexpr ll LINF = 1001002003004005006ll;
-constexpr int INF = 1001001001;
+constexpr ll INF = 1001002003004005006ll;
 constexpr int n_max = 2e5+10;
 
 template<class T>
-inline bool chmax(T &a, T b) { if(a < b) { a = b; return true; } return false; };
+inline bool chmax(T &a, T b) { if(a<b) { a=b; return true; } return false; };
 template<class T>
-inline bool chmin(T &a, T b) { if(a > b) { a = b; return true; } return false; };
+inline bool chmin(T &a, T b) { if(a>b) { a=b; return true; } return false; };
 
 template<class T, class U>
-T POW(T x, U n) {T ret = 1; while (n > 0) {if (n & 1) {ret *= x;} x *= x; n >>= 1;} return ret;};
+T POW(T x, U n) {T ret=1; while (n>0) {if (n&1) {ret*=x;} x*=x; n>>=1;} return ret;};
 
 // debug
 template <typename A, typename B>
@@ -78,35 +78,163 @@ void debug_out() {cerr << endl;};
 template<typename Head, typename... Tail>
 void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); };
 
-void Print() { cout << endl; };
-template <class T>
-void Print(vector<T> &vec) {
-  for (auto& a : vec) {cout << a; if (&a != &vec.back()) cout << " ";}
-  cout << endl;
+void LINE_OUT() {
+  cout << "--------------" << endl;
 };
-template <class T>
-void Print(vector<T> &vec, ll k){
-   ll n = vec.size();
-   k = min(k, n);
-   rep(i, 0, k-1) cout << vec[i] << " ";
-   cout << vec[k-1] << endl;
-};
-template <class T>
-void Print(vector<vector<T>> &df) {for (auto& vec : df) {print(vec);}};
-template<class T, class U>
-void Print(pair<T,U> &p){cout << p.first << " " << p.second << endl;};
 
 #ifdef LOCAL
 #define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
-#define print(...) Print(__VA_ARGS__)
+#define LINE LINE_OUT();
 #else
 #define debug(...) 71
-#define print(...) 71
+#define LINE 71;
 #endif
 
-int main() {
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << " ";
+  print(forward<Tail>(tail)...);
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+};
+
+template<std::int_fast64_t Modulus>
+class modint {
+  using i64 = int_fast64_t;
+
+  public:
+  i64 a;
+
+  constexpr modint(const i64 x = 0) noexcept {
+    this -> a = x % Modulus;
+    if(a < 0){
+      a += Modulus;
+    }
+  }
+  // constexpr i64 &value() const noexcept {return a;}
+  constexpr const i64 &value() const noexcept {return a;}
+  constexpr modint operator+(const modint rhs) const noexcept {
+    return modint(*this) += rhs;
+  }
+  constexpr modint operator-(const modint rhs) const noexcept {
+    return modint(*this) -= rhs;
+  }
+  constexpr modint operator*(const modint rhs) const noexcept {
+    return modint(*this) *= rhs;
+  }
+  constexpr modint operator/(const modint rhs) const noexcept {
+    return modint(*this) /= rhs;
+  }
+  constexpr modint &operator+=(const modint rhs) noexcept {
+    a += rhs.a;
+    if(a >= Modulus) {
+      a -= Modulus;
+    }
+    return *this;
+  }
+  constexpr modint &operator-=(const modint rhs) noexcept {
+    if(a < rhs.a) {
+      a += Modulus;
+    }
+    a -= rhs.a;
+    return *this;
+  }
+  constexpr modint &operator*=(const modint rhs) noexcept {
+    a = a * rhs.a % Modulus;
+    return *this;
+  }
+  constexpr modint &operator/=(modint rhs) noexcept {
+    i64 a_ = rhs.a, b = Modulus, u = 1, v = 0;
+    while(b){
+      i64 t = a_/b;
+      a_ -= t * b; swap(a_,b);
+      u -= t * v; swap(u,v);
+    }
+    a = a * u % Modulus;
+    if(a < 0) a += Modulus;
+    return *this;
+  }
+
+  constexpr bool operator==(const modint rhs) noexcept {
+    return a == rhs.a;
+  }
+  constexpr bool operator!=(const modint rhs) noexcept {
+    return a != rhs.a;
+  }
+  constexpr bool operator>(const modint rhs) noexcept {
+    return a > rhs.a;
+  }
+  constexpr bool operator>=(const modint rhs) noexcept {
+    return a >= rhs.a;
+  }
+  constexpr bool operator<(const modint rhs) noexcept {
+    return a < rhs.a;
+  }
+  constexpr bool operator<=(const modint rhs) noexcept {
+    return a <= rhs.a;
+  }
+  template<typename T>
+  friend constexpr modint modpow(const modint &mt, T n) noexcept {
+    if(n < 0){
+      modint t = (modint(1) / mt);
+      return modpow(t, -n);
+    }
+    modint res = 1, tmp = mt;
+    while(n){
+      if(n & 1)res *= tmp;
+      tmp *= tmp;
+      n /= 2;
+    }
+    return res;
+  }
+};
+
+const long long MOD = 998244353;
+using mint = modint<MOD>;
+// iostream
+std::ostream &operator<<(std::ostream &out, const modint<MOD> &m) {
+  out << m.a; return out;
+};
+std::istream &operator>>(std::istream &in, modint<MOD> &m) {
+  long long a; in >> a; m = mint(a); return in;
+};
+
+signed main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
+
+  int N, S; cin >> N >> S;
+  vi A(N); rep(i, 0, N) cin >> A[i];
+  vector<vector<mint>> dp(N+1, vector<mint> (S+1));
+
+  dp[0][0] = mint(1);
+  rep(i, 1, N+1) {
+    int a = A[i-1];
+    rep(j, 0, S+1) {
+      dp[i][j] = mint(2)*dp[i-1][j];
+      if (j-a >= 0) {
+        dp[i][j] += dp[i-1][j-a];
+      }
+    }
+  }
+
+  cout << dp[N][S] << endl;
 
   return 0;
 };
