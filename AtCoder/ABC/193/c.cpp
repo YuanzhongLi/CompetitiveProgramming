4aac95e7d51e4b05018865c2faa55de5e37e2a3f
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2021-02-28 14:09:32 +0900

    add abc 193

---
+++ AtCoder/ABC/193/c.cpp
@@ -0,0 +1,209 @@
+// #define LOCAL
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
+#define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+// #define M_PI 3.14159265358979323846 // CF
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // print Decimal number 10 Rank
+#define endl "\n"
+#define Case(x) printf("Case #%d: ", x); // gcj
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<char> vch;
+typedef vector<vector<char>> vvch;
+
+constexpr ll INF = 1001002003004005006ll;
+constexpr int n_max = 2e5+10;
+
+template<class T>
+inline bool chmax(T &a, T b) { if(a<b) { a=b; return true; } return false; };
+template<class T>
+inline bool chmin(T &a, T b) { if(a>b) { a=b; return true; } return false; };
+
+template<class T, class U>
+T POW(T x, U n) {T ret=1; while (n>0) {if (n&1) {ret*=x;} x*=x; n>>=1;} return ret;};
+
+// debug
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+string to_string(const string &s) {return '"' + s + '"';};
+string to_string(const char c) {return to_string((string) &c);};
+string to_string(bool b) {return (b ? "true" : "false");};
+template <size_t N>
+string to_string(bitset<N> v){
+  string res = "";
+  for(size_t i = 0; i < N; i++) res += static_cast<char>('0' + v[i]);
+  return res;
+};
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if(!first) res += ", ";
+    first = false; res += to_string(x);
+  }
+  res += "}";
+  return res;
+};
+template <typename A, typename B>
+string to_string(pair<A, B> p){return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
+
+void debug_out() {cerr << endl;};
+template<typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); };
+
+void LINE_OUT() {
+  cout << "--------------" << endl;
+};
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#define LINE LINE_OUT();
+#else
+#define debug(...) 71
+#define LINE 71;
+#endif
+
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << " ";
+  print(forward<Tail>(tail)...);
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+};
+
+const ll MAX = 1e6+5;
+
+// エラトステネスの篩
+class Sieve {
+public:
+  ll n;
+  vl f, primes;
+  Sieve() {}
+  Sieve(ll n = 1): n(n), f(n+1) {
+    f[0] = f[1] = -1ll;
+    for (ll i = 2; i <= n; i++) {
+      if (f[i]) continue;
+      primes.pb(i);
+      f[i] = i;
+      for (ll j = i*i; j <= n; j += i) {
+        if (!f[j]) f[j] = i; // jの最小の素因数
+      }
+    }
+  }
+
+  bool isPrime(int x) { return f[x] == x; }
+
+  // 素因数分解 (x > MAXの時MAX*MAXまで求められるO(1~MAXまでの素数の数))
+  vl factorList(ll x) {
+    if (x > MAX) {
+      vl res;
+      int idx = 0;
+      while (x > 1) {
+        ll p = primes[idx];
+        if (x % p == 0) {
+          x /= p;
+          res.pb(p);
+        } else {
+          idx++;
+        }
+        if (idx >= (int)primes.size()) break;
+      }
+
+      if (x > 1) res.pb(x);
+      return res;
+    } else {
+      vl res;
+      while (x != 1ll) {
+        res.pb(f[x]);
+        x /= f[x];
+      }
+      return res;
+    }
+  }
+
+  vpl factor(ll x) {
+    vl fl = factorList(x);
+    if (fl.size() == 0) return {};
+
+    vpl res(1, PLL(fl[0], 0));
+
+    for (int p: fl) {
+      if (res.back().first == p) {
+        res.back().second++;
+      } else {
+        res.emplace_back(p, 1);
+      }
+    }
+
+    return res;
+  }
+};
+
+ld ma = 1e12;
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int N; cin >> N;
+  Sieve si(MAX);
+
+  set<int> s;
+  rep(a, 2, 100005) {
+    rep(b, 2, 45) {
+      ld tmp = powl(a, b);
+      if (tmp > ma) break;
+      int alt = POW(a, b);
+      if (alt > N) break;
+      s.insert(alt);
+    }
+  }
+
+  int ans = N - s.size();
+  cout << ans << endl;
+
+
+  return 0;
+};
---
+++ AtCoder/ABC/193/e.cpp
@@ -0,0 +1,201 @@
+// #define LOCAL
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
+#define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+// #define M_PI 3.14159265358979323846 // CF
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // print Decimal number 10 Rank
+#define endl "\n"
+#define Case(x) printf("Case #%d: ", x); // gcj
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<char> vch;
+typedef vector<vector<char>> vvch;
+
+constexpr ll INF = 1001002003004005006ll;
+constexpr int n_max = 2e5+10;
+
+template<class T>
+inline bool chmax(T &a, T b) { if(a<b) { a=b; return true; } return false; };
+template<class T>
+inline bool chmin(T &a, T b) { if(a>b) { a=b; return true; } return false; };
+
+template<class T, class U>
+T POW(T x, U n) {T ret=1; while (n>0) {if (n&1) {ret*=x;} x*=x; n>>=1;} return ret;};
+
+// debug
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+string to_string(const string &s) {return '"' + s + '"';};
+string to_string(const char c) {return to_string((string) &c);};
+string to_string(bool b) {return (b ? "true" : "false");};
+template <size_t N>
+string to_string(bitset<N> v){
+  string res = "";
+  for(size_t i = 0; i < N; i++) res += static_cast<char>('0' + v[i]);
+  return res;
+};
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if(!first) res += ", ";
+    first = false; res += to_string(x);
+  }
+  res += "}";
+  return res;
+};
+template <typename A, typename B>
+string to_string(pair<A, B> p){return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
+
+void debug_out() {cerr << endl;};
+template<typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); };
+
+void LINE_OUT() {
+  cout << "--------------" << endl;
+};
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#define LINE LINE_OUT();
+#else
+#define debug(...) 71
+#define LINE 71;
+#endif
+
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << " ";
+  print(forward<Tail>(tail)...);
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+};
+
+// x mod m (m >= 1)
+long long safe_mod(long long x, long long m) {
+  x %=m; if (x < 0) x+= m;
+  return x;
+};
+
+// pair(g, x): g = gcd(a, b), xa = g (mod b), 0 <= x < b/g
+pair<long long, long long> inv_gcd(long long a, long long b) {
+  a = safe_mod(a, b);
+  if (a == 0) return {b, 0};
+
+  long long s = b, t = a;
+  long long m0 = 0, m1 = 1;
+  while (t) {
+    long long u = s/t;
+    s -= t*u;
+    m0 -= m1*u;
+    auto tmp = s;
+    s = t;
+    t = tmp;
+    tmp = m0;
+    m0 = m1;
+    m1 = tmp;
+  }
+  if (m0 < 0) m0 += b/s;
+  return {s, m0};
+};
+
+pair<long, long> crt(const vector<long long> &r, const vector<long long> &m) {
+  assert(r.size() == m.size());
+  int n = r.size();
+  long long r0 = 0, m0 = 1;
+  for (int i = 0; i < n; i++) {
+    assert(1 <= m[i]);
+    long long r1 = safe_mod(r[i], m[i]), m1 = m[i];
+    if (m0 < m1) { swap(r0, r1); swap(m0, m1); }
+    if (m0 % m1 == 0) {
+      if (r0 % m1 != r1) return {0, 0};
+      continue;
+    }
+    long long g, im;
+    tie(g, im) = inv_gcd(m0, m1);
+    long long u1 = (m1/g);
+    if ((r1-r0) % g) return {0, 0};
+    long long x = (r1-r0)/g % u1 * im % u1;
+    r0 += x * m0;
+    m0 *= u1;
+    if (r0 < 0) r0 += m0;
+  }
+  return {r0, m0};
+};
+
+void solve() {
+  int X, Y, P, Q; cin >> X >> Y >> P >> Q;
+  int a = 2*(X+Y), b = (P+Q);
+  int ans = INF;
+  rep(dy, 0, Y) {
+    int c = X+dy;
+    rep(dq, 0, Q) {
+      int d = P+dq;
+      auto pi = crt({c, d}, {a, b});
+      int t = pi.first, lcm = pi.second;
+      if (lcm == 0) continue;
+      chmin(ans, t);
+    }
+  }
+
+  if (ans == INF) {
+    cout << "infinity" << endl;
+  } else {
+    cout << ans << endl;
+  }
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int t; cin >> t;
+  while (t--) {
+    solve();
+  }
+
+  return 0;
+};
