446648ffd91d2da9507e3e126d4d8ab47aebdf04
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2021-03-04 14:43:48 +0900

    add ECR 105

---
+++ Codeforces/div2/ECR105/c.cpp
@@ -0,0 +1,199 @@
+// #define LOCAL
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
+#define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+// #define M_PI 3.14159265358979323846 // CF
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // print Decimal number 10 Rank
+#define endl "\n"
+#define Case(x) printf("Case #%d: ", x); // gcj
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<char> vch;
+typedef vector<vector<char>> vvch;
+
+constexpr ll INF = 1001002003004005006ll;
+constexpr int n_max = 2e5+10;
+
+template<class T>
+inline bool chmax(T &a, T b) { if(a<b) { a=b; return true; } return false; };
+template<class T>
+inline bool chmin(T &a, T b) { if(a>b) { a=b; return true; } return false; };
+
+template<class T, class U>
+T POW(T x, U n) {T ret=1; while (n>0) {if (n&1) {ret*=x;} x*=x; n>>=1;} return ret;};
+
+// debug
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+string to_string(const string &s) {return '"' + s + '"';};
+string to_string(const char c) {return to_string((string) &c);};
+string to_string(bool b) {return (b ? "true" : "false");};
+template <size_t N>
+string to_string(bitset<N> v){
+  string res = "";
+  for(size_t i = 0; i < N; i++) res += static_cast<char>('0' + v[i]);
+  return res;
+};
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if(!first) res += ", ";
+    first = false; res += to_string(x);
+  }
+  res += "}";
+  return res;
+};
+template <typename A, typename B>
+string to_string(pair<A, B> p){return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
+
+void debug_out() {cerr << endl;};
+template<typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); };
+
+void LINE_OUT() {
+  cout << "--------------" << endl;
+};
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#define LINE LINE_OUT();
+#else
+#define debug(...) 71
+#define LINE 71;
+#endif
+
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << " ";
+  print(forward<Tail>(tail)...);
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+};
+
+int f(vi &A, vi &B) {
+  int na = A.size();
+  int nb = B.size();
+  if (na == 0 || nb == 0) return 0;
+  set<int> set_b;
+  rep(i, 0, nb) set_b.insert(B[i]);
+  vi B_cnt(nb);
+  int i = 0;
+  int cnt = 0;
+  while (i < nb) {
+    if (i == 0) {
+      cnt++;
+      B_cnt[i] = cnt;
+    } else {
+      if (B[i] == B[i-1]+1) {
+        cnt++;
+        B_cnt[i] = cnt;
+      } else {
+        cnt = 1;
+        B_cnt[i] = cnt;
+      }
+    }
+    i++;
+  }
+
+  vi A_cnt(na+1);
+  for (int i = na-1; i >= 0; i--) {
+    if (Find(set_b, A[i])) {
+      A_cnt[i]++;
+    }
+    A_cnt[i] += A_cnt[i+1];
+  }
+
+  int ret = 0;
+  for (int i = nb-1; i>= 0; i--) {
+    int b = B[i];
+    int idx = (upper_bound(All(A), b) - A.begin()) - 1;
+    if (idx < 0) {
+      chmax(ret, A_cnt[0]);
+      break;
+    }
+    int idx2 = lower_bound(All(B), b-idx)-B.begin();
+    chmax(ret, A_cnt[idx+1]+(i-idx2+1));
+  }
+  return ret;
+};
+
+void solve() {
+  int n, m; cin >> n >> m;
+  vi pos_A, neg_A, pos_B, neg_B;
+  rep(i, 0, n) {
+    int x; cin >> x;
+    if (x > 0) pos_A.pb(x);
+    else neg_A.pb(-x);
+  }
+  rep(i, 0, m) {
+    int x; cin >> x;
+    if (x > 0) pos_B.pb(x);
+    else neg_B.pb(-x);
+  }
+  sort(All(pos_A)); sort(All(neg_A)); sort(All(pos_B)); sort(All(neg_B));
+  while (!pos_A.empty() && !pos_B.empty() && pos_A.back() > pos_B.back()) {
+    pos_A.pop_back();
+  }
+  while (!neg_A.empty() && !neg_B.empty() && neg_A.back() > neg_B.back()) {
+    neg_A.pop_back();
+  }
+  cout << f(pos_A, pos_B) + f(neg_A, neg_B) << endl;
+}
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int t; cin >> t;
+  while (t--) {
+    solve();
+    LINE;
+    LINE;
+  }
+
+  return 0;
+};
---
+++ Codeforces/div2/ECR105/sample.py
@@ -0,0 +1,4 @@
+from random import randint
+N = 1<<9
+with open('testcase.txt', mode='w') as f:
+  f.write(str(N)+'\n')
