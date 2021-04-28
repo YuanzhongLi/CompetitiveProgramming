cc8595dde08ff3cc5f80e626e5f99e1142b4bccd
YuanzhongLi <liyuanzhongutokyos1@gmail.com>
2021-04-28 17:49:24 +0900

    update

---
+++ E8_tenkei90/023.cpp
@@ -0,0 +1,318 @@
+#define LOCAL
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define rep(i,s,n) for (int i = (ll)s; i < (ll)n; i++)
+#define rrep(i,n,e) for (int i = (ll)n; i > (ll)e; i--)
+#define ll long long
+#define ld long double
+#define pb push_back
+#define eb emplace_back
+#define All(x) x.begin(), x.end()
+#define Range(x, i, j) x.begin() + i, x.begin() + j
+// #define M_PI 3.14159265358979323846 // CF
+#define deg2rad(deg) ((((double)deg)/((double)360)*2*M_PI))
+#define rad2deg(rad) ((((double)rad)/(double)2/M_PI)*(double)360)
+#define Find(set, element) set.find(element) != set.end()
+#define Decimal(x) cout << fixed << setprecision(10) << x << endl; // print Decimal number 10 Rank
+#define endl "\n"
+#define Case(x) printf("Case #%d: ", x); // gcj
+
+typedef pair<int, int> PI;
+typedef pair<ll, ll> PLL;
+typedef vector<int> vi;
+typedef vector<vector<int>> vvi;
+typedef vector<vector<vector<int>>> vvvi;
+typedef vector<ll> vl;
+typedef vector<vector<ll>> vvl;
+typedef vector<vector<vector<ll>>> vvvl;
+typedef vector<PI> vpi;
+typedef vector<vector<PI>> vvpi;
+typedef vector<PLL> vpl;
+typedef vector<vector<PLL>> vvpl;
+typedef vector<char> vch;
+typedef vector<vector<char>> vvch;
+
+constexpr ll INF = 1001002003004005006ll;
+constexpr int n_max = 2e5+10;
+
+template<class T>
+inline bool chmax(T &a, T b) { if(a<b) { a=b; return true; } return false; };
+template<class T>
+inline bool chmin(T &a, T b) { if(a>b) { a=b; return true; } return false; };
+
+template<class T, class U>
+T POW(T x, U n) {T ret=1; while (n>0) {if (n&1) {ret*=x;} x*=x; n>>=1;} return ret;};
+
+// debug
+template <typename A, typename B>
+string to_string(pair<A, B> p);
+string to_string(const string &s) {return '"' + s + '"';};
+string to_string(const char c) {return to_string((string) &c);};
+string to_string(bool b) {return (b ? "true" : "false");};
+template <size_t N>
+string to_string(bitset<N> v){
+  string res = "";
+  for(size_t i = 0; i < N; i++) res += static_cast<char>('0' + v[i]);
+  return res;
+};
+template <typename A>
+string to_string(A v) {
+  bool first = true;
+  string res = "{";
+  for(const auto &x : v) {
+    if(!first) res += ", ";
+    first = false; res += to_string(x);
+  }
+  res += "}";
+  return res;
+};
+template <typename A, typename B>
+string to_string(pair<A, B> p){return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";}
+
+void debug_out() {cerr << endl;};
+template<typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) { cerr << " " << to_string(H); debug_out(T...); };
+
+void LINE_OUT() {
+  cout << "--------------" << endl;
+};
+
+#ifdef LOCAL
+#define debug(...) cerr << "[" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#define LINE LINE_OUT();
+#else
+#define debug(...) 71
+#define LINE 71;
+#endif
+
+void print() { cout << endl; }
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(tail) != 0) cout << " ";
+  print(forward<Tail>(tail)...);
+};
+
+template <class T>
+void print(vector<T> &vec) {
+  for (auto& a : vec) {
+    cout << a;
+    if (&a != &vec.back()) cout << " ";
+  }
+  cout << endl;
+};
+
+template <class T>
+void print(vector<vector<T>> &df) {
+  for (auto& vec : df) {
+    print(vec);
+  }
+};
+
+template<std::int_fast64_t Modulus>
+class modint {
+  using i64 = int_fast64_t;
+
+  public:
+  i64 a;
+
+  constexpr modint(const i64 x = 0) noexcept {
+    this -> a = x % Modulus;
+    if(a < 0){
+      a += Modulus;
+    }
+  }
+  // constexpr i64 &value() const noexcept {return a;}
+  constexpr const i64 &value() const noexcept {return a;}
+  constexpr modint operator+(const modint rhs) const noexcept {
+    return modint(*this) += rhs;
+  }
+  constexpr modint operator-(const modint rhs) const noexcept {
+    return modint(*this) -= rhs;
+  }
+  constexpr modint operator*(const modint rhs) const noexcept {
+    return modint(*this) *= rhs;
+  }
+  constexpr modint operator/(const modint rhs) const noexcept {
+    return modint(*this) /= rhs;
+  }
+  constexpr modint &operator+=(const modint rhs) noexcept {
+    a += rhs.a;
+    if(a >= Modulus) {
+      a -= Modulus;
+    }
+    return *this;
+  }
+  constexpr modint &operator-=(const modint rhs) noexcept {
+    if(a < rhs.a) {
+      a += Modulus;
+    }
+    a -= rhs.a;
+    return *this;
+  }
+  constexpr modint &operator*=(const modint rhs) noexcept {
+    a = a * rhs.a % Modulus;
+    return *this;
+  }
+  constexpr modint &operator/=(modint rhs) noexcept {
+    i64 a_ = rhs.a, b = Modulus, u = 1, v = 0;
+    while(b){
+      i64 t = a_/b;
+      a_ -= t * b; swap(a_,b);
+      u -= t * v; swap(u,v);
+    }
+    a = a * u % Modulus;
+    if(a < 0) a += Modulus;
+    return *this;
+  }
+
+  constexpr bool operator==(const modint rhs) noexcept {
+    return a == rhs.a;
+  }
+  constexpr bool operator!=(const modint rhs) noexcept {
+    return a != rhs.a;
+  }
+  constexpr bool operator>(const modint rhs) noexcept {
+    return a > rhs.a;
+  }
+  constexpr bool operator>=(const modint rhs) noexcept {
+    return a >= rhs.a;
+  }
+  constexpr bool operator<(const modint rhs) noexcept {
+    return a < rhs.a;
+  }
+  constexpr bool operator<=(const modint rhs) noexcept {
+    return a <= rhs.a;
+  }
+  template<typename T>
+  friend constexpr modint modpow(const modint &mt, T n) noexcept {
+    if(n < 0){
+      modint t = (modint(1) / mt);
+      return modpow(t, -n);
+    }
+    modint res = 1, tmp = mt;
+    while(n){
+      if(n & 1)res *= tmp;
+      tmp *= tmp;
+      n /= 2;
+    }
+    return res;
+  }
+  friend constexpr modint modinv(const modint &rhs) noexcept {
+    i64 a_ = rhs.a, b = Modulus, u = 1, v = 0;
+    while(b){
+      i64 t = a_/b;
+      a_ -= t * b; swap(a_,b);
+      u -= t * v; swap(u,v);
+    }
+    u %= Modulus;
+    if(u < 0) u += Modulus;
+    return modint(u);
+  };
+};
+
+const long long MOD = 1e9+7;
+using mint = modint<MOD>;
+// iostream
+std::ostream &operator<<(std::ostream &out, const modint<MOD> &m) {
+  out << m.a; return out;
+};
+std::istream &operator>>(std::istream &in, modint<MOD> &m) {
+  long long a; in >> a; m = mint(a); return in;
+};
+
+
+int dy[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
+int dx[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
+int H, W;
+
+mint dfs(vvi &grid, int sy, int sx, int ey, int ex) {
+  if (!(sy <= ey && sx <= ex && 0 <= sy && ey < H && 0 <= sx && ex < W)) return 1;
+  mint ret = mint(0);
+  if (ey-sy >= ex-sx) {
+    int mid_y = (sy+ey)/2;
+    int w = ex-sx+1;
+    rep(i,0,1<<w) {
+      bool ok = true;
+      vi kings;
+      rep(j,0,w) {
+        if ((i>>j)&1) {
+          if (grid[mid_y][sx+j] || (!kings.empty() && j-kings.back() <= 1)) {
+            ok = false;
+            break;
+          } else kings.pb(j);
+        }
+      }
+
+      if (!ok) continue;
+      vpi mark;
+      for (int j: kings) {
+        int kx = j+sx;
+        rep(l,0,8) {
+          int ny = mid_y+dy[l], nx = kx+dx[l];
+          if (sy <= ny && ny <= ey && sx <= nx && nx <= ex && !grid[ny][nx]) {
+            grid[ny][nx] = 1; mark.pb({ny, nx});
+          }
+        }
+      }
+      mint alt1 = dfs(grid, sy, sx, mid_y-1, ex), alt2 = dfs(grid, mid_y+1, sx, ey, ex);
+      ret += alt1 * alt2;
+      for (auto &pi: mark) grid[pi.first][pi.second] = 0;
+    }
+    return ret;
+  } else {
+    int mid_x = (sx+ex)/2;
+    int h = ey-sy+1;
+    rep(i,0,1<<h) {
+      bool ok = true;
+      vi kings;
+      rep(j,0,h) {
+        if ((i>>j)&1) {
+          if (grid[sy+j][mid_x] || (!kings.empty() && j-kings.back() <= 1)) {
+            ok = false;
+            break;
+          } else kings.pb(j);
+        }
+      }
+
+      if (!ok) continue;
+      vpi mark;
+      for (int j: kings) {
+        int ky = j+sy;
+        rep(l,0,8) {
+          int ny = ky+dy[l], nx = mid_x+dx[l];
+          if (sy <= ny && ny <= ey && sx <= nx && nx <= ex && !grid[ny][nx]) {
+            grid[ny][nx] = 1; mark.pb({ny, nx});
+          }
+        }
+      }
+      mint alt1 = dfs(grid, sy, sx, ey, mid_x-1), alt2 = dfs(grid, sy, mid_x+1, ey, ex);
+      ret += alt1 * alt2;
+      for (auto &pi: mark) grid[pi.first][pi.second] = 0;
+    }
+    return ret;
+  }
+};
+
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  cin >> H >> W;
+  vvi grid(H, vi(W));
+  rep(i,0,H) rep(j,0,W) {
+    char ch; cin >> ch;
+    if (ch == '#') grid[i][j] = 1;
+  }
+
+  mint ans = dfs(grid, 0, 0, H-1, W-1);
+  cout << ans << endl;
+
+  return 0;
+};
